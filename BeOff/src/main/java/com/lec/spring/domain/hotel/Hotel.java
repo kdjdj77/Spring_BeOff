723e766b314098a28625d4912fd36750bcb0ebe7
JaeYoon <dhy04050@naver.com>
2022-10-17 19:19:53 +0900

    .

--- BeOff/src/main/java/com/lec/spring/domain/hotel/Hotel.java
+++ BeOff/src/main/java/com/lec/spring/domain/hotel/Hotel.java
@@ -1,5 +1,39 @@
 package com.lec.spring.domain.hotel;
 
-public class Hotel {
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.lec.spring.domain.BaseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Entity(name="db_hotel")
+public class Hotel extends BaseEntity{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String name; // 호텔 이름
+	@Column(nullable = false)
+	private String content; // 호텔 내용
+	
+	// 별점 시간 유저 지역 
+	// ID CHECK
 }
