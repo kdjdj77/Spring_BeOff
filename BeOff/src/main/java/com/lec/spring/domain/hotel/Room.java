211a5073604e2b79fc8a6781b517c7c317d4bbee
jinharyun <ajdaduy@naver.com>
2022-10-18 16:46:38 +0900

    d

--- BeOff/src/main/java/com/lec/spring/domain/hotel/Room.java
+++ BeOff/src/main/java/com/lec/spring/domain/hotel/Room.java
@@ -1,52 +1,7 @@
 package com.lec.spring.domain.hotel;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@ToString(callSuper = true)
-@Entity(name="db_room")
 public class Room {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
-	@ToString.Exclude
-	@Builder.Default  // 아래와 같이 초깃값 있으면 @Builder.Default 처리  (builder 에서 제공안함)
-	private List<Roomfile> files = new ArrayList<>();  // NPE 방지
 
-	public void addFiles(Roomfile... files) {  // xxxToMany 의 경우 만들어두면 편리
-		if(files != null) {
-			Collections.addAll(this.files, files);
-		}
-	}
-	
-	@Column(name = "hotel_id")
-	private Long hotel; // 어느 글의 첨부파일 ? (FK)
-	
-	@Column(nullable = false)
-	private int roomname;
-	@Column(nullable = false)
-	private float price;
-	private int bed;
+
 }
+
