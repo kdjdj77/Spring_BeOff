a97ff7a39db690d9978ddbddaade9247005196fe
JaeYoon <dhy04050@naver.com>
2022-10-18 16:13:35 +0900

    test

--- BeOff/src/main/java/com/lec/spring/domain/hotel/HqryResult.java
+++ BeOff/src/main/java/com/lec/spring/domain/hotel/HqryResult.java
@@ -1,5 +1,15 @@
 package com.lec.spring.domain.hotel;
 
-public class HqryResult {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HqryResult {
+	int count; // 결과값 (정수)
+	String status; // 결과 메세지
 }
