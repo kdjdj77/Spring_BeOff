128499b18d03517b9a609b58f219e6720ee93cc7
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2022-10-18 16:48:41 +0900

    Update Airtime.java

--- BeOff/src/main/java/com/lec/spring/domain/air/Airtime.java
+++ BeOff/src/main/java/com/lec/spring/domain/air/Airtime.java
@@ -1,5 +1,26 @@
 package com.lec.spring.domain.air;
 
-public class Airtime {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "db_airtime")
+public class Airtime {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length=4, nullable=false, unique=true)
+	private Long time;
 }
