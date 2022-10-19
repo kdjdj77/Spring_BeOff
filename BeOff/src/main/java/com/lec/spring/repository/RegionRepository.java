1295c7edcc2462cd5275cffac9d486bff247e555
JaeYoon <dhy04050@naver.com>
2022-10-19 20:09:16 +0900

    1019김재윤

--- BeOff/src/main/java/com/lec/spring/repository/RegionRepository.java
+++ BeOff/src/main/java/com/lec/spring/repository/RegionRepository.java
@@ -3,7 +3,8 @@ package com.lec.spring.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lec.spring.domain.Region;
+import com.lec.spring.domain.User;
 
 public interface RegionRepository extends JpaRepository<Region, Long> {
-
+	Region findByRegion(String Region);
 }
