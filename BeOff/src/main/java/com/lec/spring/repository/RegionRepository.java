052bdd1b1bf02e617e03fdd96f5f5166f6ead49e
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2022-10-19 18:52:06 +0900

    1019

--- BeOff/src/main/java/com/lec/spring/repository/RegionRepository.java
+++ BeOff/src/main/java/com/lec/spring/repository/RegionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lec.spring.domain.Region;
 
 public interface RegionRepository extends JpaRepository<Region, Long> {
-
+	Region findByRegion (String region);
 }
--- BeOff/src/main/java/com/lec/spring/repository/air/AirplaneRepository.java
+++ BeOff/src/main/java/com/lec/spring/repository/air/AirplaneRepository.java
@@ -2,8 +2,12 @@ package com.lec.spring.repository.air;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.lec.spring.domain.Region;
+import com.lec.spring.domain.air.Airname;
 import com.lec.spring.domain.air.Airplane;
+import com.lec.spring.domain.air.Airtime;
 
 public interface AirplaneRepository extends JpaRepository<Airplane, Long> {
-
+	Airplane findByDepartAndArriveAndTimeAndName(
+			Region depart, Region arrive, Airtime time, Airname name);
 }
