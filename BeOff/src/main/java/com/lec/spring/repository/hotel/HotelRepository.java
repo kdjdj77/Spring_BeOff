99ace44a3b0345c0a137f0eb1756e5bb67466dd8
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2022-10-21 20:53:55 +0900

    Create HotelRepository.java

---
+++ BeOff/src/main/java/com/lec/spring/repository/hotel/HotelRepository.java
@@ -0,0 +1,13 @@
+package com.lec.spring.repository.hotel;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.lec.spring.domain.hotel.Hotel;
+
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+    List<Hotel> findAll();
+
+}
